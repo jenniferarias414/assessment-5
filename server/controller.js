469590fd96393953f1155b7354f036b32142daac
Jenny Arias <jennyarias@Jennys-Air.attlocal.net>
2023-12-15 14:37:23 -0600

    assessment 5 update

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+const {CONNECTION_STRING} = process.env
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(CONNECTION_STRING)
 
 
 module.exports = {
